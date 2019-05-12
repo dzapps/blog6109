c964834e22970bea1fa7ccd616b58995bb7881fa
Alvin Zhan <u10100155@lssh.tp.edu.tw>
2019-05-12 14:13:58 +0800

    blog6109

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, re_path
 
 from .views import (
+    posts_home,
     posts_list,
     posts_create,
     posts_detail,
@@ -15,4 +16,5 @@ urlpatterns = [
     re_path(r'posts/(?P<slug>[\w-]+)$', posts_detail, name='detail'),
     re_path(r'posts/delete/', posts_delete),
     re_path(r'posts/', posts_list, name='list'),
+    re_path(r'', posts_home),
 ]
